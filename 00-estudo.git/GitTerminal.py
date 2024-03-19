7fc50359822529c4653c551c076bd17f323ffb71
Débora <d.sabongi@gmail.com>
2024-03-19 16:03:18 -0300

    Comandos terminal

---
+++ 00-estudo.git/GitTerminal.py
@@ -0,0 +1,81 @@
+# 1 - instalar python
+
+# “Git bash” - marcar o manter marcado 
+
+# cmd - horrivel (semelhante ao dir)
+
+# power shell
+
+# Pre-releases - beta (está sendo implementada)
+
+# COMPILADO (envia arquivo)
+
+# INTERPRETADO (executa na hora)
+
+# Controle de versão
+
+# Pontos de versão - pontos na historia do repo de codigo que arquivos fora, alterados ou adicionados (commit, tem q funcionar e ser completo)
+
+# exemplo:
+
+# https://github.com/ifspcodelab/codeodyssey-api
+
+# Fzr isso no git: 
+
+# Botão verde  - HTTPS - copiar 
+
+# split - ver correção 
+
+# issues - comentarios 
+
+# repositorio = um diretorio versionado pelo git 
+
+# txt - untracked (não rastreavel )
+
+# criar repositorio 
+
+# copiar link
+
+# colar no terminal do vs
+
+# Terminal comandos (Usar o terminal da produtividade, mais controle, automatização)
+
+# git clone - faz o dowload do repositorio git remoto 
+
+# git status - apresenta o status do repositorio local 
+
+# git add - <arquivo> add o arquivo modificado ou nn rastreado para o proximo commit
+
+# git add. - adiciona tds os arquivos modificados ou não rastreados do diretório atual
+
+# git graph - visualizar e ler o codigo 
+
+# git init - inicia um novo repo git 
+
+# git commit -m “colocar aqui oq eu alterei. descrição. PADRONALIZAÇAO”
+
+# git commit -m “Este commit faz…(pra ajudar)” 
+
+# git commit -m “adiciona comandos git (clone, status, add) no arquivo de estudo”
+
+# git remote add origin https://github.com/Sabongi08/estudos-lp3.git
+# git branch -M main
+# git push -u origin main
+# "cancelar"
+
+# modo interativo - dentro da interpretada
+
+# clear ou ctrl + L - limpar
+# : - inicio de um novo comando 
+# $ - separa o comando q vai dar
+# ~ -  diretorio(pasta) atual
+# cd - mudar diretorio
+# cd.. - voltar diretorio
+# cd~ - navegar de forma direta ou relativa escrevendo
+# . - diretorio atual (alguma coisa oculta o diretorio) 
+# pwd - atual
+# mkdir - criar diretorio
+# ls - todos os arquivos
+# ! + cod - maceta
+# ctrl + shift + v - colar
+# javac - utilizado para compilar as classes em Java
