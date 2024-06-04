776783e3a8ded969b8fc66090f1a2951c37fbf0b
Débora <d.sabongi@gmail.com>
2024-06-04 10:47:08 -0300

    modulo e pacote

---
+++ 03-modulos-pacotess/main.py
@@ -0,0 +1,10 @@
+#importa tudo de matematica, tem q colocar em td, colocando o nome do modulo
+import matematica
+
+#importar apenas os elementos (constantes, funcoes, classes)pode chamar simplesmente pelo nome 
+#necessários 
+from matematica import PI, subtrair #importante, essa q vms usar
+# from matematica import *
+# caso tenha comflito de nome
+from matematica import PI as PI_MAT  
+PI = 99999
